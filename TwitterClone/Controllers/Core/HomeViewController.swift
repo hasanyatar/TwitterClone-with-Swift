1032045839eaceb9ab8772f06e4b44e535128909
Hasan YATAR <hsnytr99@gmail.com>
2023-02-27 00:28:15 +0300

    Configure navigation bar

--- TwitterClone/Controllers/Core/HomeViewController.swift
+++ TwitterClone/Controllers/Core/HomeViewController.swift
@@ -9,6 +9,27 @@ import UIKit
 
 class HomeViewController: UIViewController {
     
+    
+    private func configureNavigationBar() {
+        let size:CGFloat = 36
+        let logoImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: size, height: size))
+        logoImageView.contentMode = .scaleAspectFill
+        logoImageView.image = UIImage(named: "twitter_logo")
+        
+        let middleView = UIView(frame: CGRect(x: 0, y: 0, width: size, height: size))
+        middleView.addSubview(logoImageView)
+        navigationItem.titleView = middleView
+        
+        let profileImage = UIImage(systemName: "person")
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: profileImage, style: .plain, target: self, action: #selector(didTapProfile))
+    }
+    
+    
+    @objc private func didTapProfile(){
+        let vc = ProfileViewController()
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+    
     private let timelineTableView: UITableView = {
         let tableView = UITableView()
         tableView.register(TweetTableViewCell.self, forCellReuseIdentifier: TweetTableViewCell.identifier)
@@ -21,6 +42,7 @@ class HomeViewController: UIViewController {
         timelineTableView.delegate = self
         timelineTableView.dataSource = self
        
+        configureNavigationBar()
     }
     
     override func viewDidLayoutSubviews() {
