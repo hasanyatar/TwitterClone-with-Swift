cab9960497b2b18f4c50fa8764a97ce498fb0fe2
Hasan YATAR <hsnytr99@gmail.com>
2023-03-08 22:05:43 +0300

    Configure firebase

--- TwitterClone/AppDelegate.swift
+++ TwitterClone/AppDelegate.swift
@@ -6,13 +6,15 @@
 //
 
 import UIKit
-
+import FirebaseCore
+import GoogleSignIn
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate{
 
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
         return true
     }
 
@@ -28,6 +30,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
+      return GIDSignIn.sharedInstance.handle(url)
     }
 
 
